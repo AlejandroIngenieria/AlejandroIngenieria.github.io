2e131809d280998f9c3f885117e608dfce8a3962
carcuz789 <53447333+carcuz789@users.noreply.github.com>
2024-06-09 11:58:11 -0600

    YA ESTA EL CST

--- src/js/ast.js
+++ src/js/ast.js
@@ -6,62 +6,14 @@ document.getElementById("play").addEventListener("click", function () {
     const container = document.getElementById("terminal");
     var cst = PEG.parse(x);
     console.log(cst);
-    const nodes = [];
-    const edges = [];
-    let idCounter = 0;
-    
-    function traverse(node, parentId = null) {
-        const nodeId = idCounter++;
-        nodes.push({ id: nodeId, label: node.value });
-    
-        if (parentId !== null) {
-            edges.push({ from: parentId, to: nodeId });
-        }
-    
-        if (Array.isArray(node.left)) {
-            for (const child of node.left) {
-                traverse(child, nodeId);
-            }
-        } else if (node.left) {
-            traverse(node.left, nodeId);
-        }
-    
-        if (Array.isArray(node.right)) {
-            for (const child of node.right) {
-                traverse(child, nodeId);
-            }
-        } else if (node.right) {
-            traverse(node.right, nodeId);
-        }
-    }
-    
-    traverse(cst);
-    
-    
-    const data = {
-        nodes: new vis.DataSet(nodes),
-        edges: new vis.DataSet(edges)
-    };
-    var options = {
-        nodes: {
-            widthConstraint: 20,
-        },        
-        layout: {
-            hierarchical: {
-                levelSeparation: 60,
-                nodeSpacing: 80,
-                parentCentralization: true,
-                direction: 'UD',        // UD, DU, LR, RL
-                sortMethod: 'directed',  // hubsize, directed
-                shakeTowards: 'roots'  // roots, leaves                        
-            },
-        },                        
-    };
-    const network = new vis.Network(container, data, options);
-
-
+    vt = new VTree(container);
+    var reader = new VTree.reader.Object();
+    var data = reader.read(cst);
+    vt.data(data)
+      .update();
 
 
 });
+
 
 
