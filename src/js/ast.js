575d23e2e0c6cfd9977a99cb662944cd15dd820e
carcuz789 <53447333+carcuz789@users.noreply.github.com>
2024-06-22 22:29:42 -0600

    22/06 termino

--- src/js/ast.js
+++ src/js/ast.js
@@ -128,37 +128,101 @@ cuadruplo.addEventListener('click', function () {
   x = editor.getValue();
   let result = PEG.parse(x);
   let cuadruplos = result.quad;
-  console.log(cuadruplos);
+ //JSON.stringify(result.quad, void 0, 2)
 
-  // Crear la tabla y el encabezado
-  const tabla = document.createElement('table');
-  tabla.classList.add('table', 'table-striped', 'table-bordered', 'table-hover');
-
-  const encabezado = tabla.createTHead();
-  const filaEncabezado = encabezado.insertRow();
-  filaEncabezado.classList.add('table-primary');
-
-  const encabezados = ['OP', 'ARG1', 'ARG2', 'ARG3', 'ARG4', 'RES'];
-  for (const encabezadoTexto of encabezados) {
-    const th = document.createElement('th');
-    th.innerText = encabezadoTexto;
-    filaEncabezado.appendChild(th);
-  }
-
-  // Agregar los datos de ArrCuad a la tabla
-  const cuerpoTabla = tabla.createTBody();
-  console.log(result);
-  for (const cuadruplo of cuadruplos) {
-    const fila = cuerpoTabla.insertRow();
-
-    fila.insertCell().innerText = cuadruplo.op ? cuadruplo.op : "";
-    fila.insertCell().innerText = cuadruplo.arg1 ? recorrer(cuadruplo.arg1) : "";
-    fila.insertCell().innerText = cuadruplo.arg2 ? recorrer(cuadruplo.arg2) : "";
-    fila.insertCell().innerText = cuadruplo.arg3 ? recorrer(cuadruplo.arg3) : "";
-    fila.insertCell().innerText = cuadruplo.arg4 ? recorrer(cuadruplo.arg4) : "";
-    fila.insertCell().innerText = cuadruplo.res ?  recorrer(cuadruplo.res) : "";
-  }
-
-  // Agregar la tabla al div con id 'terminal'
-  terminal.appendChild(tabla);
+ //document.getElementById('innertable').innerHTML = buildTable(JSON.stringify(result.quad, void 0, 2));
+ terminal.appendChild(buildTable(result.quad, void 0, 2));
+ console.log(buildTable(JSON.stringify(result.quad, void 0, 2)));
+ 
 });
+
+function buildTable(a) {
+  var e = document.createElement("table"), d, b;
+  if (isArray(a))
+      return buildArray(a);
+  for (var c in a)
+      "object" != typeof a[c] || isArray(a[c]) ? "object" == typeof a[c] && isArray(a[c]) ? (d = e.insertRow(-1),
+      b = d.insertCell(-1),
+      b.colSpan = 2,
+      b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table style="width:100%">' + $(buildArray(a[c]), !1).html() + "</table>") : (d = e.insertRow(-1),
+      b = d.insertCell(-1),
+      b.innerHTML = "<div class='td_head'>" + encodeText(c) + "</div>",
+      d = d.insertCell(-1),
+      d.innerHTML = "<div class='td_row_even'>" + encodeText(a[c]) + "</div>") : (d = e.insertRow(-1),
+      b = d.insertCell(-1),
+      b.colSpan = 2,
+      b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table style="width:100%">' + $(buildTable(a[c]), !1).html() + "</table>");
+  return e
+}
+function isArray(a) {
+  return "[object Array]" === Object.prototype.toString.call(a)
+}
+
+
+
+function buildArray(a) {
+  var e = document.createElement("table"), d, b, c = !1, p = !1, m = {}, h = -1, n = 0, l;
+  l = "";
+  if (0 == a.length)
+      return "<div></div>";
+  d = e.insertRow(-1);
+  for (var f = 0; f < a.length; f++)
+      if ("object" != typeof a[f] || isArray(a[f]))
+          "object" == typeof a[f] && isArray(a[f]) ? (b = d.insertCell(h),
+          b.colSpan = 2,
+          b.innerHTML = '<div class="td_head"></div><table style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>",
+          c = !0) : p || (h += 1,
+          p = !0,
+          b = d.insertCell(h),
+          m.empty = h,
+          b.innerHTML = "<div class='td_head'>&nbsp;</div>");
+      else
+          for (var k in a[f])
+              l = "-" + k,
+              l in m || (c = !0,
+              h += 1,
+              b = d.insertCell(h),
+              m[l] = h,
+              b.innerHTML = "<div class='td_head'>" + encodeText(k) + "</div>");
+  c || e.deleteRow(0);
+  n = h + 1;
+  for (f = 0; f < a.length; f++)
+      if (d = e.insertRow(-1),
+      td_class = isEven(f) ? "td_row_even" : "td_row_odd",
+      "object" != typeof a[f] || isArray(a[f]))
+          if ("object" == typeof a[f] && isArray(a[f]))
+              for (h = m.empty,
+              c = 0; c < n; c++)
+                  b = d.insertCell(c),
+                  b.className = td_class,
+                  l = c == h ? '<table style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>" : " ",
+                  b.innerHTML = "<div class='" + td_class + "'>" + encodeText(l) + "</div>";
+          else
+              for (h = m.empty,
+              c = 0; c < n; c++)
+                  b = d.insertCell(c),
+                  l = c == h ? a[f] : " ",
+                  b.className = td_class,
+                  b.innerHTML = "<div class='" + td_class + "'>" + encodeText(l) + "</div>";
+      else {
+          for (c = 0; c < n; c++)
+              b = d.insertCell(c),
+              b.className = td_class,
+              b.innerHTML = "<div class='" + td_class + "'>&nbsp;</div>";
+          for (k in a[f])
+              c = a[f],
+              l = "-" + k,
+              h = m[l],
+              b = d.cells[h],
+              b.className = td_class,
+              "object" != typeof c[k] || isArray(c[k]) ? "object" == typeof c[k] && isArray(c[k]) ? b.innerHTML = '<table style="width:100%">' + $(buildArray(c[k]), !1).html() + "</table>" : b.innerHTML = "<div class='" + td_class + "'>" + encodeText(c[k]) + "</div>" : b.innerHTML = '<table style="width:100%">' + $(buildTable(c[k]), !1).html() + "</table>"
+      }
+  return e
+}
+function encodeText(a) {
+  return $("<div />").text(a).html()
+
+}
+function isEven(a) {
+  return 0 == a % 2
+}
